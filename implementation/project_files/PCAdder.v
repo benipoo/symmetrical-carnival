77a020c78f621588469f688ea065caecf504bfb2
Benjamin Feaster <55043306+benipoo@users.noreply.github.com>
2023-04-22 00:28:57 -0400

    Update PCAdder.v

--- implementation/project_files/PCAdder.v
+++ implementation/project_files/PCAdder.v
@@ -1,30 +1,29 @@
-`timescale 1ns / 1ps
+timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    18:30:44 10/18/2021 
-// Design Name: 
-// Module Name:    PCAdder 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
+Company :
+Engineer :
 //
-// Dependencies: 
+Create Date : 18 : 30 : 44 10/18/2021
+Design Name :
+Module Name : PCAdder
+Project Name :
+Target Devices :
+Tool versions :
+Description :
 //
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
+Dependencies :
+//
+Revision :
+Revision 0.01 - FILE Created
+Additional Comments :
 //
 //////////////////////////////////////////////////////////////////////////////////
 module PCAdder(
-input [15:0] PCIn,
-output reg [15:0] PCOut
+input [15 : 0] PCIn,
+output reg [15 : 0] PCOut
 );
-always @ (*)
-begin
+always @ ( *)
+BEGIN
 	PCOut = PCIn + 2;
-end
+END
 endmodule
-
